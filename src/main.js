4b52700feaa8de5b5e9f2a45c61da9c156b2b578
josue <josuefloresbonilla335@gmail.com>
2023-10-25 22:11:44 -0600

    nueva actualización para la tinda en linea

--- src/main.js
+++ src/main.js
@@ -45,3 +45,18 @@ realizarOrdenButton.addEventListener('click', () => {
     const total = carrito.calcularTotal();
     alert(`Orden de compra realizada. Total: $${total}`);
 });
+// Agregar un listener para los botones "Eliminar" en la lista de productos
+const removeFromListButtons = document.querySelectorAll('.remove-from-list');
+
+removeFromListButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const name = button.getAttribute('data-name');
+        // Puedes agregar aquí la lógica para eliminar el producto de la lista
+        // Por ejemplo, puedes ocultar el elemento o mostrar un mensaje de confirmación
+        const productElement = document.querySelector(`[data-name="${name}"]`);
+        if (productElement) {
+            productElement.style.display = 'none'; // Oculta el producto
+            // También puedes mostrar un mensaje de confirmación aquí
+        }
+    });
+});
